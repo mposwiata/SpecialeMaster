fbee2e94570a82593a14543b94b89e84c758a888
mposwiata <martinposwiata@gmail.com>
2020-08-24 16:00:26 +0200

    Initial Commit

---
+++ SpecialeMaster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpecialeMaster
+//
+//  Created by Martin Poswiata on 24/08/2020.
+//  Copyright © 2020 Martin Poswiata. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
