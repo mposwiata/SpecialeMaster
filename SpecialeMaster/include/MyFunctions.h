9a376bd4bc7a098083eb7da1d9ac3606057c914e
mposwiata <martinposwiata@gmail.com>
2020-09-01 20:04:40 +0200

    First python step

---
+++ SpecialeMaster/include/MyFunctions.h
@@ -0,0 +1,13 @@
+//
+//  MyFunctions.h
+//  SpecialeMaster
+//
+//  Created by Martin Poswiata on 24/08/2020.
+//  Copyright © 2020 Martin Poswiata. All rights reserved.
+//
+
+#ifndef MyFunctions_h
+#define MyFunctions_h
+
+
+#endif /* MyFunctions_h */
