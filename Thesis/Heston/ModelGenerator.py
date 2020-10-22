9b1e026cf9e10751f0c1a8eaab820cd2f31c1c07
mposwiata <martinposwiata@gmail.com>
2020-10-22 21:09:43 +0200

    Please work

--- Thesis/Heston/ModelGenerator.py
+++ Thesis/Heston/ModelGenerator.py
@@ -60,16 +60,14 @@ filteredImpVolSingle = np.reshape(filteredImpVolSingle, (-1, 1))
 
 cpu_cores = cpu_count()
 parallel_list = [
-    """
-    [model_input, gridPriceOutput, 4, 100, "HestonGridPriceAll_100"],
-    [model_input, gridPriceOutput, 4, 1000, "HestonGridPriceAll_1000"],
-    [model_input, gridImpVolOutput, 4, 100, "HestonGridImpVolAll_100"],
-    [model_input, gridImpVolOutput, 4, 1000, "HestonGridImpVolAll_100"],
-    [filteredGridModelPrice, filteredGridPrice, 4, 100, "HestonGridPriceFilter_100"],
-    [filteredGridModelPrice, filteredGridPrice, 4, 1000, "HestonGridPriceFilter_1000"],
-    [filteredGridModelImpVol, filteredGridImpVol, 4, 100, "HestonGridImpVolFilter_100"],
-    [filteredGridModelImpVol, filteredGridImpVol, 4, 1000, "HestonGridImpVolFilter_1000"],
-    """
+    #[model_input, gridPriceOutput, 4, 100, "HestonGridPriceAll_100"],
+    #[model_input, gridPriceOutput, 4, 1000, "HestonGridPriceAll_1000"],
+    #[model_input, gridImpVolOutput, 4, 100, "HestonGridImpVolAll_100"],
+    #[model_input, gridImpVolOutput, 4, 1000, "HestonGridImpVolAll_100"],
+    #[filteredGridModelPrice, filteredGridPrice, 4, 100, "HestonGridPriceFilter_100"],
+    #[filteredGridModelPrice, filteredGridPrice, 4, 1000, "HestonGridPriceFilter_1000"],
+    #[filteredGridModelImpVol, filteredGridImpVol, 4, 100, "HestonGridImpVolFilter_100"],
+    #[filteredGridModelImpVol, filteredGridImpVol, 4, 1000, "HestonGridImpVolFilter_1000"],
     [singleInput, singlePrice_output, 4, 100, "HestonSinglePriceAll_100"],
     [singleInput, singlePrice_output, 4, 1000, "HestonSinglePriceAll_1000"],
     [singleInput, singleImpVol_output, 4, 100, "HestonSingleImpVolAll_100"],
