11cc646b8b5bea0b44cce97220ba37f711ea45d0
mposwiata <martinposwiata@gmail.com>
2020-11-28 17:51:26 +0100

    ...

--- Thesis/Heston/ModelGenerator.py
+++ Thesis/Heston/ModelGenerator.py
@@ -195,5 +195,5 @@ if __name__ == '__main__':
         cpu_cores = int(min(cpu_count()/4, 16))
 
     pool = Pool(cpu_cores)
-    res = pool.starmap(mg.NN_mc_model_1, next_list_2, chunksize=1)
+    res = pool.starmap(mg.NN_mc_model_1, next_list_3, chunksize=1)
     pool.close()
