c4892ad1cb2cba343b04d108a27c61f64258a988
mposwiata <martinposwiata@gmail.com>
2020-12-03 15:31:38 +0100

    mc price

--- Thesis/Heston/MC_al.py
+++ Thesis/Heston/MC_al.py
@@ -702,33 +702,33 @@ if __name__ == "__main__":
     mc_100_4_1000_dict = model_grads(mc_100_4_1000, input_good_easy, input_good_hard, some_option)
 
     mc_prediction_data = {
-        #"mc_1000_price_5_1000" : [mc_1000_price_5_1000_dict["pred"][0], mc_1000_price_5_1000_dict["pred"][1]],
-        #"mc_10_price_5_50" : [mc_10_price_5_50_dict["pred"][0], mc_10_price_5_50_dict["pred"][1]],
-        #"mc_10000_price_3_1000" : [mc_10000_price_3_1000_dict["pred"][0], mc_10000_price_3_1000_dict["pred"][1]],
-        #"mc_100_price_4_500" : [mc_100_price_4_500_dict["pred"][0], mc_100_price_4_500_dict["pred"][1]],
-        #"mc_1_price_2_1000" : [mc_1_price_2_1000_dict["pred"][0], mc_1_price_2_1000_dict["pred"][1]],
+        "mc_1000_price_5_1000" : [mc_1000_price_5_1000_dict["pred"][0], mc_1000_price_5_1000_dict["pred"][1]],
+        "mc_10_price_5_50" : [mc_10_price_5_50_dict["pred"][0], mc_10_price_5_50_dict["pred"][1]],
+        "mc_10000_price_3_1000" : [mc_10000_price_3_1000_dict["pred"][0], mc_10000_price_3_1000_dict["pred"][1]],
+        "mc_100_price_4_500" : [mc_100_price_4_500_dict["pred"][0], mc_100_price_4_500_dict["pred"][1]],
+        "mc_1_price_2_1000" : [mc_1_price_2_1000_dict["pred"][0], mc_1_price_2_1000_dict["pred"][1]],
         "mc_10000_5_1000" : [mc_10000_5_1000_dict["pred"][0], mc_10000_5_1000_dict["pred"][1]],
         "mc_1000_5_100" : [mc_1000_5_100_dict["pred"][0], mc_1000_5_100_dict["pred"][1]],
         "mc_100_4_1000" : [mc_100_4_1000_dict["pred"][0], mc_100_4_1000_dict["pred"][1]]
     }
 
     mc_delta_data= {
-        #"mc_1000_price_5_1000" : [mc_1000_price_5_1000_dict["delta"][0], mc_1000_price_5_1000_dict["delta"][1]],
-        #"mc_10_price_5_50" : [mc_10_price_5_50_dict["delta"][0], mc_10_price_5_50_dict["delta"][1]],
-        #"mc_10000_price_3_1000" : [mc_10000_price_3_1000_dict["delta"][0], mc_10000_price_3_1000_dict["delta"][1]],
-        #"mc_100_price_4_500" : [mc_100_price_4_500_dict["delta"][0], mc_100_price_4_500_dict["delta"][1]],
-        #"mc_1_price_2_1000" : [mc_1_price_2_1000_dict["delta"][0], mc_1_price_2_1000_dict["delta"][1]],
+        "mc_1000_price_5_1000" : [mc_1000_price_5_1000_dict["delta"][0], mc_1000_price_5_1000_dict["delta"][1]],
+        "mc_10_price_5_50" : [mc_10_price_5_50_dict["delta"][0], mc_10_price_5_50_dict["delta"][1]],
+        "mc_10000_price_3_1000" : [mc_10000_price_3_1000_dict["delta"][0], mc_10000_price_3_1000_dict["delta"][1]],
+        "mc_100_price_4_500" : [mc_100_price_4_500_dict["delta"][0], mc_100_price_4_500_dict["delta"][1]],
+        "mc_1_price_2_1000" : [mc_1_price_2_1000_dict["delta"][0], mc_1_price_2_1000_dict["delta"][1]],
         "mc_10000_5_1000" : [mc_10000_5_1000_dict["delta"][0], mc_10000_5_1000_dict["delta"][1]],
         "mc_1000_5_100" : [mc_1000_5_100_dict["delta"][0], mc_1000_5_100_dict["delta"][1]],
         "mc_100_4_1000" : [mc_100_4_1000_dict["delta"][0], mc_100_4_1000_dict["delta"][1]]
     }
 
     mc_gamma_data = {
-        #"mc_1000_price_5_1000" : [mc_1000_price_5_1000_dict["gamma"][0], mc_1000_price_5_1000_dict["gamma"][1]],
-        #"mc_10_price_5_50" : [mc_10_price_5_50_dict["gamma"][0], mc_10_price_5_50_dict["gamma"][1]],
-        #"mc_10000_price_3_1000" : [mc_10000_price_3_1000_dict["gamma"][0], mc_10000_price_3_1000_dict["gamma"][1]],
-        #"mc_100_price_4_500" : [mc_100_price_4_500_dict["gamma"][0], mc_100_price_4_500_dict["gamma"][1]],
-        #"mc_1_price_2_1000" : [mc_1_price_2_1000_dict["gamma"][0], mc_1_price_2_1000_dict["gamma"][1]],
+        "mc_1000_price_5_1000" : [mc_1000_price_5_1000_dict["gamma"][0], mc_1000_price_5_1000_dict["gamma"][1]],
+        "mc_10_price_5_50" : [mc_10_price_5_50_dict["gamma"][0], mc_10_price_5_50_dict["gamma"][1]],
+        "mc_10000_price_3_1000" : [mc_10000_price_3_1000_dict["gamma"][0], mc_10000_price_3_1000_dict["gamma"][1]],
+        "mc_100_price_4_500" : [mc_100_price_4_500_dict["gamma"][0], mc_100_price_4_500_dict["gamma"][1]],
+        "mc_1_price_2_1000" : [mc_1_price_2_1000_dict["gamma"][0], mc_1_price_2_1000_dict["gamma"][1]],
         "mc_10000_5_1000" : [mc_10000_5_1000_dict["gamma"][0], mc_10000_5_1000_dict["gamma"][1]],
         "mc_1000_5_100" : [mc_1000_5_100_dict["gamma"][0], mc_1000_5_100_dict["gamma"][1]],
         "mc_100_4_1000" : [mc_100_4_1000_dict["gamma"][0], mc_100_4_1000_dict["gamma"][1]]
